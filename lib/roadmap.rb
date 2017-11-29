0a9e59a4d0267f983ae0bfd075bfd8979ec88bd1
Sanyan Sharar <raad34@gmail.com>
2017-11-28 21:54:30 -0500

    roadmapchkpnts

---
+++ lib/roadmap.rb
@@ -0,0 +1,9 @@
+module Roadmap
+    def get_roadmap(roadmap_id)
+      response = JSON.parse(Kele.get("/roadmaps/#{roadmap_id}", headers: { "authorization" => @post_response}).body)
+    end
+  
+    def get_checkpoint(checkpoint_id)
+      response = Kele.get("/checkpoints/#{checkpoint_id}", headers: { "authorization" => @post_response})
+    end
+end 
